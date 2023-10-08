33327e8bb91ad58f8e4e72281afbc310644e3f8e
Konstantinos Zais <thereal.coding.wookiee@gmail.com>
2023-10-08 12:24:02 +0200

    create user service

---
+++ src/main/kotlin/de/patronus/challenge/user/business/UserService.kt
@@ -0,0 +1,11 @@
+package de.patronus.challenge.user.business
+
+import org.springframework.stereotype.Service
+
+@Service
+class UserService {
+    fun createUser(user: User): User {
+        TODO("Not yet implemented")
+    }
+
+}
