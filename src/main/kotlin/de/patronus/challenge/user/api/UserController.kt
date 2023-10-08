8385546a19f984397ddb9e78b8bab393f41fe927
Konstantinos Zais <thereal.coding.wookiee@gmail.com>
2023-10-08 19:23:24 +0200

    add dto validation

--- src/main/kotlin/de/patronus/challenge/user/api/UserController.kt
+++ src/main/kotlin/de/patronus/challenge/user/api/UserController.kt
@@ -2,14 +2,14 @@ package de.patronus.challenge.user.api
 
 import de.patronus.challenge.ChallengeApplication
 import de.patronus.challenge.user.business.UserService
-import org.springframework.http.HttpStatus
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.*
+
 
 @RestController
 @RequestMapping(ChallengeApplication.SERVER_URL_PATH_API + "/user")
@@ -20,11 +20,23 @@ class UserController(
 
     @PostMapping
     fun createUser(
-        @RequestBody userDto: UserDTO
+        @RequestBody @Valid userDto: UserDTO
     ): ResponseEntity<UserDTO> {
         val user = mapper.toModel(userDto)
         val createdUser = userService.createUser(user)
 
         return ResponseEntity.status(CREATED).body(mapper.toDto(createdUser))
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(BAD_REQUEST)
+    fun handleConstraintViolationException(e: MethodArgumentNotValidException): ResponseEntity<HashMap<String, String?>> {
+        val errors = HashMap<String, String?>()
+        e.bindingResult.allErrors.forEach { error ->
+            val fieldName = (error as FieldError).field
+            val errorMessage: String? = error.defaultMessage
+            errors[fieldName] = errorMessage
+        }
+        return ResponseEntity.status(BAD_REQUEST).body(errors)
     }
 }
--- src/main/kotlin/de/patronus/challenge/user/api/UserDTO.kt
+++ src/main/kotlin/de/patronus/challenge/user/api/UserDTO.kt
@@ -1,17 +1,21 @@
 package de.patronus.challenge.user.api
 
 import java.time.LocalDate
+import jakarta.validation.constraints.NotEmpty
+
 
 data class UserDTO(
-    val firstName: String,
-    val lastName: String,
-    val address: AddressDTO,
-    val birthday: LocalDate
+    @field:NotEmpty(message = "NO_FIRST_NAME_PROVIDED")
+    val firstName: String?,
+    @field:NotEmpty(message = "NO_LAST_NAME_PROVIDED")
+    val lastName: String?,
+    val address: AddressDTO?,
+    val birthday: LocalDate?
 )
 
 data class AddressDTO(
-    val street: String,
-    val houseNumber: String,
-    val city: String,
-    val postalCode: String
+    val street: String?,
+    val houseNumber: String?,
+    val city: String?,
+    val postalCode: String?
 )
