d8b08cde1b946ac84ea43cbd098d7587c5e639f9
Konstantinos Zais <thereal.coding.wookiee@gmail.com>
2023-10-09 17:42:02 +0200

    add logic to user service for assigning and test

--- src/main/kotlin/de/patronus/challenge/user/business/UserService.kt
+++ src/main/kotlin/de/patronus/challenge/user/business/UserService.kt
@@ -1,13 +1,22 @@
 package de.patronus.challenge.user.business
 
+import de.patronus.challenge.device.business.Device
+import de.patronus.challenge.device.business.DeviceService
 import de.patronus.challenge.user.persistence.UserPersistenceService
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class UserService(
-    private val userPerstistenceService: UserPersistenceService
+    private val userPersistenceService: UserPersistenceService,
+    private val deviceService: DeviceService
 ) {
     fun createUser(user: User): User {
-        return userPerstistenceService.createUser(user)
+        return userPersistenceService.createUser(user)
+    }
+
+    fun assignDevice(device: Device, userId: UUID): User {
+        deviceService.validateDeviceAssignment(device.id!!)
+        return userPersistenceService.assignDeviceToUser(device, userId)
     }
 }
--- src/test/kotlin/de/patronus/challenge/user/business/UserServiceTest.kt
+++ src/test/kotlin/de/patronus/challenge/user/business/UserServiceTest.kt
@@ -1,7 +1,12 @@
 package de.patronus.challenge.user.business
 
+import de.patronus.challenge.device.DeviceFixture.createDevice
+import de.patronus.challenge.device.business.DeviceService
+import de.patronus.challenge.user.UserFixture
+import de.patronus.challenge.user.UserFixture.ID
 import de.patronus.challenge.user.UserFixture.createUser
 import de.patronus.challenge.user.persistence.UserPersistenceService
+import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
@@ -12,6 +17,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 @ExtendWith(MockKExtension::class)
 class UserServiceTest {
     @RelaxedMockK lateinit var userPersistenceService: UserPersistenceService
+    @RelaxedMockK lateinit var deviceService: DeviceService
     @InjectMockKs lateinit var sut: UserService
 
     @Test
@@ -23,5 +29,13 @@ class UserServiceTest {
         verify { userPersistenceService.createUser(user) }
     }
 
+    @Test
+    fun `should passs down to persistence layer for assigning device to user, if device valid`() {
+        val device = createDevice()
+        every { deviceService.validateDeviceAssignment(any()) } returns true
 
+        sut.assignDevice(device, ID)
+
+        verify { userPersistenceService.assignDeviceToUser(device, ID) }
+    }
 }
