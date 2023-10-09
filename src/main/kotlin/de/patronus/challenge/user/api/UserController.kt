022ef2645c39d5f6bd73951ebfbf147d193824d1
Konstantinos Zais <thereal.coding.wookiee@gmail.com>
2023-10-09 16:39:32 +0200

    add endpoint for adding device to user

--- src/main/kotlin/de/patronus/challenge/user/api/UserController.kt
+++ src/main/kotlin/de/patronus/challenge/user/api/UserController.kt
@@ -1,31 +1,46 @@
 package de.patronus.challenge.user.api
 
 import de.patronus.challenge.ChallengeApplication
+import de.patronus.challenge.device.api.DeviceDTO
+import de.patronus.challenge.device.api.DeviceDTOMapper
 import de.patronus.challenge.user.business.UserService
 import jakarta.validation.Valid
-import org.springframework.http.HttpStatus.BAD_REQUEST
-import org.springframework.http.HttpStatus.CREATED
+import org.springframework.http.HttpStatus.*
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.*
+import java.util.*
+import kotlin.collections.HashMap
 
 
 @RestController
 @RequestMapping(ChallengeApplication.SERVER_URL_PATH_API + "/users")
 class UserController(
     private val userService: UserService,
-    private val mapper: UserDTOMapper
+    private val userMapper: UserDTOMapper,
+    private val deviceMapper: DeviceDTOMapper
 ) {
 
     @PostMapping
     fun createUser(
         @RequestBody @Valid userDto: UserDTO
     ): ResponseEntity<UserDTO> {
-        val user = mapper.toModel(userDto)
+        val user = userMapper.toModel(userDto)
         val createdUser = userService.createUser(user)
 
-        return ResponseEntity.status(CREATED).body(mapper.toDto(createdUser))
+        return ResponseEntity.status(CREATED).body(userMapper.toDto(createdUser))
+    }
+
+    @PutMapping("/{userId}/devices")
+    fun assignDevice(
+        @PathVariable userId: UUID,
+        @RequestBody @Valid device: DeviceDTO
+    ): ResponseEntity<UserDTO> {
+        val device = deviceMapper.toModel(device)
+        val updatedUser = userService.assignDevice(device, userId)
+
+       return ResponseEntity.status(OK).body(userMapper.toDto(updatedUser))
     }
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
